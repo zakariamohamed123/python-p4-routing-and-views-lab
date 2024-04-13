0c437b262009756a3d4f7ab3304efab6e6e96fd2
zakariamohamed123 <zakaria.mohamed1@student.moringaschool.com>
2024-04-14 00:10:12 +0300

    completed

---
+++ server/app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)
+    return param
+
+@app.route('/count/<int:param>')
+def count(param):
+    numbers = '<br>'.join(str(num) for num in range(param))
+    return numbers
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return 'Invalid operation'
+    return str(result)
+
+if __name__ == '__main__':
+    app.run()
